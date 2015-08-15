364907c83d7f016972f7bbeade0a729d25a9d096
Josh Morrow <josh@mydomino.com>
2015-08-15 00:02:26 -0700

    Little refactoring all over the place

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,26 +1,3 @@
 class RobotsController < ApplicationController
-  def move_right
-    @robot = Robot.find(params[:robot_id])
-    @robot.move_right
-    @board = @robot.board
-    redirect_to @board
-  end
-  def move_down
-    @robot = Robot.find(params[:robot_id])
-    @robot.move_down
-    @board = @robot.board
-    redirect_to @board
-  end
-  def move_up
-    @robot = Robot.find(params[:robot_id])
-    @robot.move_up
-    @board = @robot.board
-    redirect_to @board
-  end
-  def move_left
-    @robot = Robot.find(params[:robot_id])
-    @robot.move_left
-    @board = @robot.board
-    redirect_to @board
-  end
+  
 end
