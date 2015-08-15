77522df891b69102da6d2783388422d62e21f645
Josh Morrow <josh@mydomino.com>
2015-08-14 22:25:43 -0700

    Terrible JS, but it works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,48 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+var selected_robot = '';
+$(document).ready(function(){
+  $('.robot').click(function(e){
+    selected_robot = $(e.target);
+    $('.robot').removeClass('selected');
+    selected_robot.addClass('selected');
+    selected_robot.space = selected_robot.parent('.space');
+    $('input#move_robot_id').val(selected_robot.attr('id'));
+  });
+
+  $('.space').click(function(e)
+  {
+    space = $(e.target);
+    if(selected_robot.space.data('row') == space.data("row"))
+    {
+      if(selected_robot.space.data('column') < space.data('column'))
+      {
+        $('input#move_direction').val('right');
+        submit_move_form();
+      }
+      else if(selected_robot.space.data('column') > space.data('column'))
+      {
+        $('input#move_direction').val('left')
+        submit_move_form();
+      }
+    }
+    if(selected_robot.space.data('column') == space.data('column'))
+    {
+      if(selected_robot.space.data('row') < space.data('row'))
+      {
+        $('input#move_direction').val('down')
+        submit_move_form();
+      }
+      else if(selected_robot.space.data('row') > space.data('row'))
+      {
+        $('input#move_direction').val('up')
+        submit_move_form();
+      }
+    }
+  })
+});
+function submit_move_form()
+{
+  $('form.new_move').submit();
+}
