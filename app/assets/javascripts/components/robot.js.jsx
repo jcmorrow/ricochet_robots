ffe1131c4ad9db182a4863bf2609c6d2a4c0376d
Josh Morrow <josh@mydomino.com>
2015-12-15 14:45:34 -0800

    Revert "More react"
    This reverts commit a237309a98df2328dcfe78065872e91d48f08c57.

--- app/assets/javascripts/components/robot.js.jsx
+++ app/assets/javascripts/components/robot.js.jsx
@@ -1,13 +1,7 @@
 var Robot = React.createClass({
   render: function(){
-    var colors = {
-      red: '#FF3333',
-      green: '#33FF33',
-      blue: '#3333FF',
-      yellow: '#FFFF33'
-    }
     var robotStyle = {
-      backgroundColor: colors[this.props.data.color]
+      backgroundColor: this.props.data.color
     }
     return (
       <div className="robot" style={robotStyle}></div>
--- app/assets/javascripts/components/space.js.jsx
+++ app/assets/javascripts/components/space.js.jsx
@@ -2,7 +2,11 @@ var Space = React.createClass({
   getInitialState: function(){
     if(this.props.robot != null)
     {
-      var robot = <Robot data={this.props.robot} />;
+      var robot = <Robot data={this.props.robot} />
+    }
+    else
+    {
+      var robot = '';
     }
     return({
       robot: robot
